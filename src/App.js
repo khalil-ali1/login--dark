634466763da059f2f8856a17d2af08299cfcb32b
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-06-14 18:32:18 +0100

    a

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="app">
-      <div className="login">
-        <h1>login</h1>
-        <div className="container">
-          <div className="top">
+    <div className="flex justify-center bg-zinc-500 ">
+    <div className="w-[80%] bg-slate-600 rounded-3xl">
+      <div className="text-center">
+        <h1 className="my-6 font-extrabold ">LOGIN</h1>
+        <div className="">
+          <div className="space-x-4 ">
             <i className="fab fa-google"></i>
             <i className="fab fa-facebook-square"></i>
             <i className="fab fa-linkedin"></i>
             <i className="fab fa-twitter-square"></i>
             <i className="fab fa-apple"></i>
             <div>
-              <p className="divider">
-                <span>or</span>
+              <p className="border-b-2 mb-10">
+                <span className="border bg-slate-50">or</span>
               </p>
-              <form>
-                <label>E-mail</label>
+              <form className="border ">
+                <label className="pl-5">E-mail</label>
                 <input type="email" placeholder="Enter your email" />
+                <br />
                 <label>password</label>
                 <input type="password" placeholder="Enter your password" />
                 <div className="remember">
@@ -42,6 +44,7 @@ function App() {
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
