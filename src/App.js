c2ad6d4fc87a273d79fba06e6d159e0b385fb495
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-06-15 05:32:34 +0100

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="app flex justify-center text-xl  text-center transition-all transition-0.5 mt-4 ">
       <div className="login w-[25%]">
         <h1 className="mt-20 mb-10 font-extrabold text-center">LOGIN</h1>
-        <div className="container border rounded-3xl shadow-2xl m-5 ">
+        <div className="contain flex flex-col justify-center border rounded-3xl shadow-2xl m-5 ">
           <div className="top flex px-[20%] justify-between my-8">
             <i className="fab fa-google"></i>
             <i className="fab fa-facebook-square"></i>
@@ -15,17 +15,17 @@ function App() {
             <i className="fab fa-twitter-square"></i>
             <i className="fab fa-apple"></i>
           </div>
-          <p className="divider"><span>or</span></p>
-          <form className="my-4 flex flex-col border">
+          <p className="divider border-b w-[100%] text-center leading-4  "><span className="px-8 bg-white">or</span></p>
+          <form className="my-4 flex flex-col ">
             <label className="">E-mail</label>
-            <input type="email" placeholder="Enter your email" className="" />
+            <input type="email" placeholder="Enter your email" className="border my-2 mx-[15%] text-center" />
             <label>password</label>
-            <input type="password" placeholder="Enter your password" className="" />
-            <div className="remember flex justify-center space-x-2">
+            <input type="password" placeholder="Enter your password" className="border my-2 mx-[15%] text-center" />
+            <div className="remember my-2 flex justify-center space-x-2 w-[100%]">
               <input type="checkbox" checked="checked" />
               <p>Remember Me</p>
             </div>
-            <button>Log In</button>
+            <button className="border rounded-full bg-purple-400 mx-[25%]">Log In</button>
           </form>
           <div className="bottom">
             <p>Forget your password?</p>
